0693337024ef3da6b025c399aa2f096ea3a0a98a
Alperen Yalcin <alperenyalcin212@gmail.com>
2025-04-27 16:49:38 +0300

    fix: Set random_state in LogisticRegression for reproducibility

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
 X_train_vec = vectorizer.fit_transform(X_train_text)
 X_test_vec = vectorizer.transform(X_test_text)
 
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(max_iter=1000, random_state=42)
 model.fit(X_train_vec, y_train_labels)
 
 y_pred = model.predict(X_test_vec)
