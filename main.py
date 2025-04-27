0e5d0076281ae83ee86cc1406ff28838d6b8cbb3
Alperen Yalcin <alperenyalcin212@gmail.com>
2025-04-27 16:46:09 +0300

    feat: Evaluate the model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 import pandas as pd
 import tensorflow_datasets as tfds
 import re
@@ -50,3 +50,18 @@ vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
  
 X_train_vec = vectorizer.fit_transform(X_train_text)
 X_test_vec = vectorizer.transform(X_test_text)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train_vec, y_train_labels)
+
+y_pred = model.predict(X_test_vec)
+accuracy = accuracy_score(y_test_labels, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+print("\nClassification Report: ")
+print(classification_report(y_test_labels, y_pred))
+
+print("\nConfusion Matrix: ")
+print(confusion_matrix(y_test_labels, y_pred))
+
+
